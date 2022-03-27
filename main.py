2d437c4aa64edbe7da03646b63c94625c71ca01e
Vinishk0 <94686362+Vinishk0@users.noreply.github.com>
2022-03-27 18:49:22 +0500

    Созданна база данных и таблица user

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+import sqlite3
 
 import os
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
-
+connection = sqlite3.connect('BotZozhnik.db')
+cur = connection.cursor()
 
 @dp.message_handler(commands=['start', 'help'])
 async def command_start(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Помощь!')
+    if not cur.execute(f'''select chat_id From users
+                        where chat_id = '{message.chat.id}' ''').fetchall():
+        cur.execute("INSERT INTO users(chat_id, name, weight, city)"
+                    "VALUES(?, ?, ?, ?)",
+                    (message.chat.id, str(message.from_user.first_name), None, None))
+        connection.commit()
 
 
 @dp.message_handler()
