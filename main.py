0a683a08fc49a7eb6314c089349730d8a613cf56
Vinishk0 <94686362+Vinishk0@users.noreply.github.com>
2022-03-29 00:33:42 +0500

    Добавленна функция изменения города

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from config import TOKEN
 import sqlite3
 import os
 from random import choice
+from bot_fiels import keyboard_markup as kb
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -12,7 +13,7 @@ connection = sqlite3.connect('BotZozhnik.db')
 cur = connection.cursor()
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def command_start(message: types.Message):
     if not cur.execute(f'''select chat_id From users
                         where chat_id = '{message.chat.id}' ''').fetchall():
@@ -22,11 +23,30 @@ async def command_start(message: types.Message):
         connection.commit()
 
 
+@dp.message_handler(commands=['choicecity'])
+async def command_start(message: types.Message):
+    try:
+        new_city = message.get_args()
+        cur.execute(f"UPDATE users SET city='{new_city}' "
+                    f"WHERE chat_id={message.from_user.id}")
+        connection.commit()
+    except:
+        await bot.send_message(message.from_user.id, 'Некоректный запрос')
+    else:
+        await bot.send_message(message.from_user.id, f'Ваш город был изменён на: {new_city}')
+
+
 @dp.message_handler(text='Мотивация')
 async def quotes(message: types.Message):
     f = open("quotes.txt", 'r', encoding="utf8")
     data = f.readlines()
     await bot.send_message(message.from_user.id, str(choice(data)))
+
+
+@dp.message_handler(text=['Поменять город'])
+async def weather_kb(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Чтобы изменить город введите "/choicecity <<Ваш город>>"',
+                           reply_markup=kb.weatherMenu)
 
 
 executor.start_polling(dp, skip_updates=True)
