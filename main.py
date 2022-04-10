74b2ccaf69cc14bdf7ed858e17bf55567680e89c
Vinishk0 <94686362+Vinishk0@users.noreply.github.com>
2022-04-10 21:12:19 +0500

    команд /settings, команда /stop, возможность включать/выключать уведомления погоды

--- main.py
+++ main.py
@@ -6,23 +6,24 @@ import sqlite3
 from time import sleep
 import os
 from random import choice
-# from bot_fiels import keyboard_markup as kb
+from bot_fiels import keyboard_markup as kb
 from aiogram.types import ReplyKeyboardMarkup
 
 user_data = {}
-exercises = ['Прыжки', 'Приседание у стены', 'Отжимания от пола', 'Подъемы на стул', 'Наклон вперед из положения лежа', 'Приседания', 'Бег, колени вверх',
+exercises = ['Прыжки', 'Приседание у стены', 'Отжимания от пола', 'Подъемы на стул', 'Наклон вперед из положения лежа',
+             'Приседания', 'Бег, колени вверх',
              'Выпады', 'Отжимания с поворотом', 'Боковая планка', 'Обратные отжимания от стула', 'Планка']
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 connection = sqlite3.connect('BotZozhnik.db')
 cur = connection.cursor()
 
-def get_keyboard(index=0):
+
+def get_keyboard():
     buttons = [
         types.InlineKeyboardButton(text="<<", callback_data="back"),
-        types.InlineKeyboardButton(text=exercises[index], callback_data="None"),
-        types.InlineKeyboardButton(text=">>", callback_data="next"),
-        types.InlineKeyboardButton(text="Подтвердить", callback_data="confirm")
+        types.InlineKeyboardButton(text="Подтвердить", callback_data="confirm"),
+        types.InlineKeyboardButton(text=">>", callback_data="next")
     ]
     keyboard = types.InlineKeyboardMarkup(row_width=3)
     keyboard.add(*buttons)
@@ -33,9 +34,9 @@ def get_keyboard(index=0):
 async def command_start(message: types.Message):
     if not cur.execute(f'''select chat_id From users
                         where chat_id = '{message.chat.id}' ''').fetchall():
-        cur.execute("INSERT INTO users(chat_id, name, weight, city)"
-                    "VALUES(?, ?, ?, ?)",
-                    (message.chat.id, str(message.from_user.first_name), None, None))
+        cur.execute("INSERT INTO users(chat_id, name, weight, city, mailing)"
+                    "VALUES(?, ?, ?, ?, ?)",
+                    (message.chat.id, str(message.from_user.first_name), None, None, 'False'))
         connection.commit()
 
 
@@ -52,6 +53,20 @@ async def command_start(message: types.Message):
         await bot.send_message(message.from_user.id, f'Ваш город был изменён на: {new_city}')
 
 
+@dp.message_handler(commands=['stop'])
+async def command_start(message: types.Message):
+    btn = types.InlineKeyboardButton(text="ДА!", callback_data="stop")
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    keyboard.add(btn)
+    await bot.send_message(message.from_user.id, f'Вы действительно хотите остановить бота?', reply_markup=keyboard)
+
+
+@dp.message_handler(commands=['settings'])
+async def command_start(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Вы перешли в настройки',
+                           reply_markup=kb.settingsMenu)
+
+
 @dp.message_handler(text='Мотивация')
 async def quotes(message: types.Message):
     f = open("quotes.txt", 'r', encoding="utf8")
@@ -64,20 +79,35 @@ async def quotes(message: types.Message):
 #     await bot.send_message(message.from_user.id, 'Чтобы изменить город введите "/choicecity <<Ваш город>>"',
 #                            reply_markup=kb.weatherMenu)
 
+@dp.message_handler(text='Уведомления погоды')
+async def notification_weather(message: types.Message):
+    btn = types.InlineKeyboardButton(text="Включить", callback_data="notification_weather_on")
+    btn1 = types.InlineKeyboardButton(text="Выключить", callback_data="notification_weather_off")
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(btn, btn1)
+    await bot.send_message(message.from_user.id, f'Уведомления погоды', reply_markup=keyboard)
+
+
+@dp.message_handler(text='Уведомление остановки бота')
+async def notification_completion(message: types.Message):
+    btn = types.InlineKeyboardButton(text="Включить", callback_data="notification_completion_on")
+    btn1 = types.InlineKeyboardButton(text="Выключить", callback_data="notification_completion_off")
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(btn, btn1)
+    await bot.send_message(message.from_user.id, f'Уведомление остановки бота', reply_markup=keyboard)
+
 
 @dp.message_handler(text='Тренировки')
 async def workout(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(*['Начать тренировку', 'Случайное упражнение'])
-    keyboard.add(*['Выбрать упражнение', 'Назад в "Другое"'])
-    await bot.send_message(message.from_user.id, 'Вы перешли в раздел "Тренировки", введите команду или выберите из предложенных',
-                           reply_markup=keyboard)
+    await bot.send_message(message.from_user.id,
+                           'Вы перешли в раздел "Тренировки", введите команду или выберите из предложенных',
+                           reply_markup=kb.exerciseMenu)
 
 
 @dp.message_handler(text='Выбрать упражнение')
 async def choice_exercise(message: types.Message):
     user_data[message.from_user.id] = 0
-    await bot.send_message(message.from_user.id, 'Выберите упражнение:',
+    await bot.send_message(message.from_user.id, f'Упражнение: {exercises[user_data[message.from_user.id]]}',
                            reply_markup=get_keyboard())
 
 
@@ -93,10 +123,42 @@ async def random_exercise(message: types.Message):
 @dp.message_handler(text='Начать тренировку')
 async def workout_start(message: types.Message):
     btn = types.InlineKeyboardButton(text="ДА!", callback_data="yes")
-    keyboard = types.InlineKeyboardMarkup(row_width=3)
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
     keyboard.add(btn)
     await bot.send_message(message.from_user.id, f'Начинаем?', reply_markup=keyboard)
 
+
+
+@dp.callback_query_handler(text='notification_completion_on')
+async def notification_completion_on(call: types.CallbackQuery):
+    await call.message.edit_text('Уведомления о погоде были включены')
+    cur.execute(f"UPDATE users SET mailing='True' "
+                f"WHERE chat_id={call.from_user.id}")
+    connection.commit()
+
+
+@dp.callback_query_handler(text='notification_completion_off')
+async def notification_completion_off(call: types.CallbackQuery):
+    await call.message.edit_text('Уведомления о погоде были включены')
+    cur.execute(f"UPDATE users SET mailing='True' "
+                f"WHERE chat_id={call.from_user.id}")
+    connection.commit()
+
+
+@dp.callback_query_handler(text='notification_weather_on')
+async def notification_weather_on(call: types.CallbackQuery):
+    await call.message.edit_text('Уведомления о погоде были включены')
+    cur.execute(f"UPDATE users SET mailing='True' "
+                f"WHERE chat_id={call.from_user.id}")
+    connection.commit()
+
+
+@dp.callback_query_handler(text='notification_weather_off')
+async def notification_weather_off(call: types.CallbackQuery):
+    await call.message.edit_text('Уведомления о погоде были выключены')
+    cur.execute(f"UPDATE users SET mailing='False' "
+                f"WHERE chat_id={call.from_user.id}")
+    connection.commit()
 
 
 @dp.callback_query_handler(text='yes')
@@ -113,28 +175,30 @@ async def yes(call: types.CallbackQuery):
     await call.answer()
 
 
+@dp.callback_query_handler(text='stop')
+async def countdown(call: types.CallbackQuery):
+    if call.from_user.id == 1212339097 or call.from_user.id == 1300485082:
+        await call.message.edit_text('Бот остановлен!')
+        await bot.answer_callback_query(call.id,
+                                        text='Бот был принудительно остановлен!'
+                                             ' Данное оповещение пришло всем пользователям.',
+                                        show_alert=True)
+        data = cur.execute(f'''SELECT chat_id From users''').fetchall()
+        for i in data:
+            await bot.send_message(*i, f'Бот был остановлен, извените за неудобства😔')
+        exit(0)
+    else:
+        await call.message.edit_text('У вас нет прав алминистратора!')
+
 
 @dp.callback_query_handler(text='back')
 async def countdown(call: types.CallbackQuery):
     user_index = user_data[call.from_user.id]
     try:
-        await call.message.edit_text(f'Выберите упражнение:', reply_markup=get_keyboard(user_index - 1))
+        await call.message.edit_text(f'Упражнение: {exercises[user_index - 1]}', reply_markup=get_keyboard())
         user_data[call.from_user.id] = user_index - 1
     except IndexError:
-        await call.message.edit_text(f'Выберите упражнение:', reply_markup=get_keyboard(11))
-        user_data[call.from_user.id] = 11
-    await call.answer()
-
-
-
-@dp.callback_query_handler(text='back')
-async def callbacks_bcak(call: types.CallbackQuery):
-    user_index = user_data[call.from_user.id]
-    try:
-        await call.message.edit_text(f'Выберите упражнение:', reply_markup=get_keyboard(user_index - 1))
-        user_data[call.from_user.id] = user_index - 1
-    except IndexError:
-        await call.message.edit_text(f'Выберите упражнение:', reply_markup=get_keyboard(11))
+        await call.message.edit_text(f'Упражнение: {exercises[11]}', reply_markup=get_keyboard())
         user_data[call.from_user.id] = 11
     await call.answer()
 
@@ -143,10 +207,10 @@ async def callbacks_bcak(call: types.CallbackQuery):
 async def callbacks_next(call: types.CallbackQuery):
     user_index = user_data[call.from_user.id]
     try:
-        await call.message.edit_text(f'Выберите упражнение:', reply_markup=get_keyboard(user_index + 1))
+        await call.message.edit_text(f'Упражнение: {exercises[user_index + 1]}', reply_markup=get_keyboard())
         user_data[call.from_user.id] = user_index + 1
     except IndexError:
-        await call.message.edit_text(f'Выберите упражнение:', reply_markup=get_keyboard(0))
+        await call.message.edit_text(f'Упражнение: {exercises[0]}', reply_markup=get_keyboard())
         user_data[call.from_user.id] = 0
     await call.answer()
 
@@ -159,5 +223,6 @@ async def callbacks_confirm(call: types.CallbackQuery):
     f.close()
     await call.message.edit_text(f'Упражнение "{exercises[user_index]}"\n{data[user_index]}')
     await call.answer()
+
 
 executor.start_polling(dp, skip_updates=True)
